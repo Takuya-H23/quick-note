862290bcc65449784ff04d4071c456ada6b2323e
Takuya Hirata <takuyahirata4@gmail.com>
2021-12-22 21:46:53 -0500

    Add NoteCard

---
+++ app/components/NoteCard.tsx
@@ -0,0 +1,35 @@
+import { BiClipboard } from 'react-icons/bi'
+
+export default function NoteCard({
+  title,
+  copy
+}: {
+  id: string
+  title: string
+  copy: string
+}) {
+  const onClick = () => {
+    const type = 'text/plain'
+    const blob = new Blob([copy], { type })
+    navigator.clipboard.write([new ClipboardItem({ [type]: blob })]).then(
+      x => {
+        console.log('good', x)
+      },
+      e => {
+        console.error(e)
+      }
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <h3 className="font-medium text-lg">{title}</h3>
+      <div className="flex justify-between items-center">
+        <span>{copy}</span>{' '}
+        <button type="button" onClick={onClick}>
+          <BiClipboard className="w-6 h-6" />
+        </button>
+      </div>
+    </div>
+  )
+}
