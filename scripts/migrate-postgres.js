71a987ea29c35135d144cf52c987dc9771e121e5
Takuya Hirata <takuyahirata4@gmail.com>
2022-01-03 11:17:46 -0500

    Config: update migration scripts

--- scripts/migrate-postgres.js
+++ scripts/migrate-postgres.js
@@ -4,15 +4,27 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
+const env = process.env.NODE_ENV || 'development'
+
+const getProps = isProd => (acc, x) => {
+  const [key, value] = Object.entries(x)[0]
+  return isProd
+    ? { ...acc, [key]: process.env[value.concat('_PROD')] }
+    : { ...acc, [key]: process.env[value] }
+}
+
+const config = [
+  { password: 'POSTGRES_PASSWORD' },
+  { user: 'POSTGRES_USER' },
+  { host: 'POSTGRES_HOST' },
+  { port: 'POSTGRES_PORT' },
+  { database: 'POSTGRES_DATABASE' }
+].reduce(getProps(process.env.NODE_ENV === 'production'), {})
+
+console.log(`Running migration on ${env}...`)
+
 async function run() {
-  console.log('Running migration...')
-  const client = new Client({
-    password: process.env.POSTGRES_PASSWORD,
-    user: 'postgres',
-    host: 'localhost',
-    port: 6428,
-    database: 'quick_note_dev'
-  })
+  const client = new Client(config)
 
   client.connect()
 
