30e4a319d54e27efbb0d9b8c2aed74336001fc41
Takuya Hirata <takuyahirata4@gmail.com>
2022-01-06 08:33:03 -0500

    Fix: updaet submenu style

--- app/routes/notes/details/$noteId.tsx
+++ app/routes/notes/details/$noteId.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
 import { Link, useLoaderData, json } from 'remix'
-import { Dialog, Menu } from '@headlessui/react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
 import { PencilAltIcon, DotsVerticalIcon } from '@heroicons/react/outline'
 import { AiOutlineDelete } from 'react-icons/ai'
 
@@ -49,63 +49,111 @@ export default function NoteDetail() {
 
   return (
     <div>
-      <Dialog
-        onClose={handleClose}
-        open={isOpen}
-        className="fixed inset-0 z-10 overflow-y-auto"
-      >
-        <Dialog.Overlay className="fixed inset-0" />
-        <div className="fixed w-8/12  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <div className="bg-gray-50 p-5 rounded-md">
-            <Dialog.Title className="font-medium">
-              Are you sure you want to delete this note?
-            </Dialog.Title>
-            <div className="flex gap-x-4 p-6 overflow-hidden justify-center">
-              <button onClick={handleClose}>Cancel</button>
-              <form method="post" action={`/notes/details/${id}/delete`}>
-                <input
-                  type="hidden"
-                  name="redirectTo"
-                  value={`/notes/${folderId}`}
-                />
-                <button type="submit" className="flex gap-x-2">
-                  Yes
-                </button>
-              </form>
-            </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={handleClose}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <div className="bg-gray-50 p-5 rounded-md">
+                  <Dialog.Title className="font-medium">
+                    Are you sure you want to delete this note?
+                  </Dialog.Title>
+                  <div className="flex gap-x-4 p-6 overflow-hidden justify-center">
+                    <button onClick={handleClose}>Cancel</button>
+                    <form method="post" action={`/notes/details/${id}/delete`}>
+                      <input
+                        type="hidden"
+                        name="redirectTo"
+                        value={`/notes/${folderId}`}
+                      />
+                      <button type="submit" className="flex gap-x-2">
+                        Yes
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
           </div>
-        </div>
-      </Dialog>
+        </Dialog>
+      </Transition>
       <SnackBar message={message} variant={variant} />
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold">{title}</h2>
-        <Menu as="div" className="relative">
+        <Menu as="div" className="relative inline-block text-left">
           <Menu.Button className="flex items-center">
             <DotsVerticalIcon className="w-6 h-6" />
           </Menu.Button>
-          <Menu.Items className="z-10 absolute right-0 w-56 mt-2 origin-top-right bg-gray-600 rounded-sm shadow-lg">
-            <div className="p-2 flex flex-col gap-y-4">
-              <Menu.Item>
-                {() => (
-                  <Link to="edit" className="flex gap-x-2 items-center">
-                    <PencilAltIcon className="h-5 w-5" /> <span>Edit</span>
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {() => (
-                  <button
-                    type="button"
-                    className="flex gap-x-2 items-center"
-                    onClick={handleOpen}
-                  >
-                    <AiOutlineDelete className="w-5 h-5" />
-                    <span>Delete</span>
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="z-10 absolute  right-0 w-56 mt-2 origin-top-right bg-gray-700 rounded-sm shadow-lg divide-y divide-gray-100 ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="p-2 flex flex-col gap-y-4 text-gray-50">
+                <Menu.Item>
+                  {({ active }) => (
+                    <Link
+                      to="edit"
+                      className={`${
+                        active ? 'bg-violet-500 ' : ''
+                      } group flex rounded-md items-center w-full px-2 py-2 text-sm flex gap-x-2 items-center`}
+                    >
+                      <PencilAltIcon className="h-5 w-5" /> <span>Edit</span>
+                    </Link>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      type="button"
+                      className={`${
+                        active ? 'bg-violet-500 ' : ''
+                      } group flex rounded-md items-center w-full px-2 py-2 text-sm flex gap-x-2 items-center`}
+                      onClick={handleOpen}
+                    >
+                      <AiOutlineDelete className="w-5 h-5" />
+                      <span>Delete</span>
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
         </Menu>
       </div>
       {copy && <CopyText copy={copy} className="mt-4" />}
